d89acd88391653753474c993e809f463194ec630
takanome2024 <galangrambuanarki12@gmail.com>
2026-07-30 18:58:02 +0700

    integrasi

--- BE/src/server.js
+++ BE/src/server.js
@@ -3,10 +3,20 @@ import routes from "./routes/index.js";
 import { errorHandler } from "./middleware/error.middleware.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./docs/swagger.js";
+import cors from "cors";
+
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("==================================");
+  console.log(`${req.method} ${req.originalUrl}`);
+  console.log("BODY:", req.body);
+  console.log("AUTH:", req.headers.authorization);
+  next();
+});
 
 // Swagger
 app.use(
@@ -25,10 +35,3 @@ app.listen(3000, () => {
   console.log("🚀 Server running at http://localhost:3000");
 });
 
-app.use((req, res, next) => {
-  console.log("==================================");
-  console.log(`${req.method} ${req.originalUrl}`);
-  console.log("BODY:", req.body);
-  console.log("AUTH:", req.headers.authorization);
-  next();
-});
